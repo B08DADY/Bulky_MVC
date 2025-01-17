99f5f2af34a84819ebbbf86d042cd4812f30f496
Amr Boghdady <amrbo8dady@gmail.com>
2025-01-17 09:20:56 +0200

    N-Tier Architicture

--- Bulky.Models/Category.cs
+++ Bulky.Models/Category.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-
-namespace BulkyWeb.Models
+namespace Bulky.Models
 {
     public class Category
     {
--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,11 +1,11 @@
-using BulkyWeb.Data;
+using Bulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSingleton<;
 
 
 builder.Services.AddDbContext<ApllicationDbContext>(options =>
